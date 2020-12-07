e0fd629f26dc2ddf909c22e573f274e82cea29bb
jessicamenius <37314892+jessicamenius@users.noreply.github.com>
2020-12-07 12:02:09 -0600

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
+import Landing from "./pages/Landing"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exactpath="/landing">
+            <Landing/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-  
+
 export default App;
